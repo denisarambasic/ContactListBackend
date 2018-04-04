e65d2edcd5bf792aa83f20b7c15f4c889d234990
denisarambasic <denis.arambasic@gmail.com>
2018-04-05 00:46:03 +0200

    Created a JSON endpoint toget all users

--- app/Typeqast/Controllers/UserController.php
+++ app/Typeqast/Controllers/UserController.php
@@ -2,12 +2,27 @@
 
 namespace Typeqast\Controllers;
 
+use Typeqast\Models\User;
+
 class UserController
 {
 	
 	public function getAllUsers()
 	{
-		echo 'Here We deliver all users in json';
+		
+		$user = new User();
+		$users = $user->getAllUsers();
+		$user_temp = [];
+		
+		//Get the phones for every user
+		foreach($users as $u){
+			$user_temp[] = array_merge($u, ['phones' => $user->getPhonesByUser($u['id'])]);
+		}
+		
+		http_response_code(200);
+		header('Content-type: application/json');
+		echo json_encode($user_temp);
+
 	}
 	
 }
