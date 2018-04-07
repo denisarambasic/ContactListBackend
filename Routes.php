f60627a1dd2ff810374fc48a96b79c871a213569
denisarambasic <denis.arambasic@gmail.com>
2018-04-07 20:18:53 +0200

    added a new route for deleting a user

--- Routes.php
+++ Routes.php
@@ -11,11 +11,12 @@ $routes->setRoute('OPTIONS', '/api/users/create', 'UserController', 'createUser'
 
 //Get user by id
 $routes->setRoute('GET', '/api/users', 'UserController', 'getUserById', ['id']);
-//Update user by id
+//Update user (favorite field) by id
 $routes->setRoute('PATCH', '/api/users', 'UserController', 'updateUserById', ['id']);
+//delete user
+$routes->setRoute('DELETE', '/api/users', 'UserController', 'deleteUser', ['id']);
 //to handle preflight request
 $routes->setRoute('OPTIONS', '/api/users', 'UserController', 'updateUserById', ['id']);
-
 
 /*=== check if clients request match any route ===*/
 $routes->callController();
