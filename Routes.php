36ee4e3448dfd611b03ad38b86969bdaad7c5da2
denisarambasic <denis.arambasic@gmail.com>
2018-04-07 18:20:21 +0200

    added a new PATCH route to update the favorites field for the given user

--- Routes.php
+++ Routes.php
@@ -9,6 +9,13 @@ $routes->setRoute('GET', '/api/users', 'UserController', 'getAllUsers', []);
 $routes->setRoute('POST', '/api/users/create', 'UserController', 'createUser', []);
 $routes->setRoute('OPTIONS', '/api/users/create', 'UserController', 'createUser', []);
 
+//Get user by id
+$routes->setRoute('GET', '/api/users', 'UserController', 'getUserById', ['id']);
+//Update user by id
+$routes->setRoute('PATCH', '/api/users', 'UserController', 'updateUserById', ['id']);
+//to handle preflight request
+$routes->setRoute('OPTIONS', '/api/users', 'UserController', 'updateUserById', ['id']);
+
 
 /*=== check if clients request match any route ===*/
 $routes->callController();
