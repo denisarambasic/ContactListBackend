31de73340aa778d76c9e0d243cf32137cbde2848
denisarambasic <denis.arambasic@gmail.com>
2018-04-07 15:36:38 +0200

    create a new user(contact) and handle image upload

--- app/Typeqast/Controllers/UserController.php
+++ app/Typeqast/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController
 	
 	public function getAllUsers()
 	{
-		
+		header('Access-Control-Allow-Origin: *');
 		$user = new User();
 		$users = $user->getAllUsers();
 		$user_temp = [];
@@ -23,6 +23,50 @@ class UserController
 		header('Content-type: application/json');
 		echo json_encode($user_temp);
 
+	}
+	
+	public function createUser()
+	{
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Methods: GET, POST');
+		header('Content-type: application/json');
+		
+		$first_name 	= filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_STRING);
+		$last_name 		= filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_STRING);
+		$image_name		= time() . $_FILES['image_name']['name'];
+		$email 			= filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+		$favorite		= ($_POST['favorite'] == "true") ? 1 : 0;
+		$phones			= $_POST['phones'];
+		
+		//upload file
+		$this->upload($image_name);
+				
+		//*Insert a new user(contact) into users table;
+		$user = new User();
+		$user_id = $user->addNewUser($first_name, $last_name, $image_name, $email, $favorite);
+	
+		
+		//Insert phones for that user (contact) into phones tbl
+		foreach($phones as $phone)
+		{
+			$data = json_decode($phone);
+			$user->insertPhone($user_id, filter_var($data->name, FILTER_SANITIZE_STRING), filter_var($data->number, FILTER_SANITIZE_STRING));
+		}
+		
+
+	}
+	
+	/*=== A helper method to upload a file in the img/ folder ===*/
+	public function upload($filename){
+		
+		$upload_dir = dirname(__DIR__, 3).'/img/';
+		
+	
+		if (move_uploaded_file($_FILES['image_name']['tmp_name'], $upload_dir. $filename)) {
+			echo json_encode(['mess'=>"Uploaded"]);
+		} else {
+			echo json_encode(['mess'=>"File was not uploaded"]);
+		}
 	}
 	
 }
