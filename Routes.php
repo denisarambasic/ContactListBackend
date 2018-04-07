9c3db7f653e70dfe7cec4f63408dd6954683f5b1
denisarambasic <denis.arambasic@gmail.com>
2018-04-07 15:32:13 +0200

    Added the same route but this time to handle the OPTIONS req Method to solve the preflight request error

--- Routes.php
+++ Routes.php
@@ -6,6 +6,8 @@ $routes = new Routing();
 
 /*=== Here we add all our application routes ===*/
 $routes->setRoute('GET', '/api/users', 'UserController', 'getAllUsers', []);
+$routes->setRoute('POST', '/api/users/create', 'UserController', 'createUser', []);
+$routes->setRoute('OPTIONS', '/api/users/create', 'UserController', 'createUser', []);
 
 
 /*=== check if clients request match any route ===*/
